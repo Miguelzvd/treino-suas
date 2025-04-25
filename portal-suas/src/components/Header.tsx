06a617842b16b6f2c2400012ef18e321dcad4252
renzozuk <renzo.zuk19@gmail.com>
2025-04-25 20:13:39 -0300

    Refactor: HeaderIcon

--- portal-suas/src/components/Header.tsx
+++ portal-suas/src/components/Header.tsx
@@ -5,24 +5,36 @@ import HeaderIcon from "./HeaderIcon";
 const boxWidth = "100vw";
 const paddingH = "10vw";
 
+const iconSize = 33;
+
+const navbarPaths = [
+    { label: "COPAS", path: "#" },
+    { label: "VIGSA/CAD ÚNICO", path: "#" },
+    { label: "FEAS", path: "#" },
+    { label: "PSB", path: "#" },
+    { label: "PSE", path: "#" },
+    { label: "GTEP", path: "#" },
+    { label: "APOIO TÉCNICO", path: "#" },
+    { label: "CEAS", path: "#" },
+    { label: "CIB", path: "#" },
+];
+
+const iconsPaths = [
+    { destinationPath: "#", imagePath: "search_icon.png" },
+    { destinationPath: "#", imagePath: "contrast_icon.png" },
+    { destinationPath: "#", imagePath: "profile_icon.png" },
+];
+
 const Header = () => (
     <VStack>
         <HStack bg="white" w={boxWidth} h="100px" padding={`0 ${paddingH}`}>
-            <Image src="SUAS_Logo.png" />
+            <Link href="#">
+                <Image src="SUAS_Logo.png" />
+            </Link>
         </HStack>
         <HStack bg="suas.green.main" w={boxWidth} h="69px" padding={`0 ${paddingH}`} justifyContent="space-between">
             <HStack>
-                {[
-                    { label: "COPAS", path: "#" },
-                    { label: "VIGSA/CAD ÚNICO", path: "#" },
-                    { label: "FEAS", path: "#" },
-                    { label: "PSB", path: "#" },
-                    { label: "PSE", path: "#" },
-                    { label: "GTEP", path: "#" },
-                    { label: "APOIO TÉCNICO", path: "#" },
-                    { label: "CEAS", path: "#" },
-                    { label: "CIB", path: "#" },
-                ].map((item, index) => (
+                {navbarPaths.map((item, index) => (
                     <Flex key={item.path} align="center">
                         {index !== 0 && <Text color="white" mx={2}>|</Text>}
                         <Link as={NavLink} href={item.path} mx={2} fontWeight="medium" color="white">
@@ -31,10 +43,10 @@ const Header = () => (
                     </Flex>
                 ))}
             </HStack>
-            <HStack>
-                <HeaderIcon destinationPath="#" imagePath="search_icon.png" />
-                <HeaderIcon destinationPath="#" imagePath="dark_mode_icon.png" />
-                <HeaderIcon destinationPath="#" imagePath="profile_icon.png" />
+            <HStack w={`${iconSize * 1.5 * iconsPaths.length}px`} justifyContent="space-between">
+                {iconsPaths.map((item) => (
+                    <HeaderIcon iconSize={iconSize} destinationPath={item.destinationPath} imagePath={item.imagePath} />
+                ))}
             </HStack>
         </HStack>
     </VStack>
--- portal-suas/src/components/HeaderIcon.tsx
+++ portal-suas/src/components/HeaderIcon.tsx
@@ -1,13 +1,14 @@
 import { Image, Link } from "@chakra-ui/react";
 
 interface HeaderIconProps {
+    iconSize: number;
     destinationPath: string;
     imagePath: string;
 }
 
-const HeaderIcon = ({ destinationPath, imagePath }: HeaderIconProps) => (
+const HeaderIcon = ({ iconSize, destinationPath, imagePath }: HeaderIconProps) => (
     <Link href={destinationPath}>
-        <Image width="33px" height="33px" marginLeft="15px" src={imagePath} />
+        <Image width={`${iconSize}px`} height={`${iconSize}px`} src={imagePath} />
     </Link>
 );
 
